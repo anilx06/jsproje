c9c11490acc74cf5b3f22c8f2c3ab40baf5f3691
anilx06 <anildeniz998@gmail.com>
2024-02-10 22:34:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1> KULLANICI KAYIT </h1>
+
+
+Kullanıcı giriş ve kayıt olma 
+
+
+<h2> EKRAN GÖRÜNTÜSÜ </h2>
+
+
+![](2024-02-10-13-03-57.gif)
---
+++ script.js
@@ -0,0 +1,11 @@
+const logregbox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', () => {
+    logregbox.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    logregbox.classList.remove('active');
+});
